5d6de7c5f7aa4fa153606b889b31644ad68b4731
Ray001-sudo <bensonray25@gmail.com>
2026-06-05 16:51:01 +0300

    Update main.py

--- alerting/main.py
+++ alerting/main.py
@@ -1,7 +1,7 @@
-import asyncio
-from alerting.rule_engine import app, refresh_rules
+from alerting.rule_engine import app
+
+def main() -> None:
+    app.main()
 
 if __name__ == '__main__':
-    # Initial load of rules
-    asyncio.run(refresh_rules())
-    app.main()
+    main()
